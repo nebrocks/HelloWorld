47c7cdb3a2adb58d8f6a85ba606c5038f8137c4b
nebrocks <nebalgos.com>
2021-05-10 22:15:27 +0530

    modified files

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,11 +2,17 @@ class HelloWorld{
 	public static void main(String... args){
 		System.out.println("HelloWorld");
 		go();
+		set();
 		System.out.println("Main end..");
 	}
 	
 	public static void go(){
 		System.out.println("Go method got called..");
+		
+	}
+	
+	public static void set(){
+		System.out.println("Set method got called..");
 		
 	}
 }
