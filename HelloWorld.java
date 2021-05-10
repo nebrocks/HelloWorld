0093f82cb6330deadd22841c60375107e255b89d
nebrocks <nebalgos.com>
2021-05-10 22:55:08 +0530

    modified files

--- HelloWorld.java
+++ HelloWorld.java
@@ -8,4 +8,9 @@ class HelloWorld{
 	public static void bubbleSort(){
 		System.out.println("Bubble sort is completed.");
 	}
+	
+	public static void mergeSort(){
+		
+		System.out.println("Merge sort is completed. ");
+	}
 }
